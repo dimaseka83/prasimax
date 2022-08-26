7a1cd99868c8eabc5eab2d5fdfd5612c62fe8f88
dhimas <dimaseka83@gmail.com>
2022-08-26 19:54:23 +0700

    ++

--- src/components/FooterGuest.vue
+++ src/components/FooterGuest.vue
@@ -1,65 +1,74 @@
 <template>
     <div>
-         <v-container fluid class=" white--text background-footer pa-16">
+        <v-img src="@/assets/images/dummy/rectangle.svg"></v-img>
+        <v-container fluid class="white--text background-footer pa-16">
             <v-row>
                 <v-col md="2">
-                    <v-img src="@/assets/images/logo.png"></v-img>
+                    <v-img src="@/assets/images/white_logo.png"></v-img>
                 </v-col>
             </v-row>
             <v-row>
                 <v-col>
-                    <p class="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere non earum veniam sapiente necessitatibus mollitia omnis harum ullam asperiores nam voluptatibus cum impedit tempore quibusdam distinctio officiis blanditiis, officia rem!</p>
+                    <p class="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere non earum
+                        veniam sapiente necessitatibus mollitia omnis harum ullam asperiores nam voluptatibus cum
+                        impedit tempore quibusdam distinctio officiis blanditiis, officia rem!</p>
                     <p class="display-1 font-weight-bold text-uppercase">alamat</p>
                     <div>
-                        <span>Perkantoran Taman Melati Margonda</span><br/>
-                        <span>Jl. Margonda Raya 525A</span><br/>
-                        <span>Depok 16424</span><br/>
-                        <span>INDONESIA</span><br/><br/>
-                        <span>Telp: +6221-7888-0672</span><br/>
+                        <span>Perkantoran Taman Melati Margonda</span><br />
+                        <span>Jl. Margonda Raya 525A</span><br />
+                        <span>Depok 16424</span><br />
+                        <span>INDONESIA</span><br /><br />
+                        <span>Telp: +6221-7888-0672</span><br />
                     </div>
                 </v-col>
                 <v-col>
                     <p class="display-1 font-weight-bold text-capitalize">hubungi kami</p>
                     <p>
-                        <span>Technical Support: </span><br/>
-                        <span>service@prasimax.com</span><br/>
+                        <span>Technical Support: </span><br />
+                        <span>service@prasimax.com</span><br />
                     </p>
                     <p>
-                        <span>Penjualan dan Kerja Sama Bisnis: </span><br/>
-                        <span>sales@prasimax.com</span><br/>
+                        <span>Penjualan dan Kerja Sama Bisnis: </span><br />
+                        <span>sales@prasimax.com</span><br />
                     </p>
                     <v-row class="mt-16">
                         <v-col md="4" v-for="(social, i) in socials" :key="i + 'social2'">
-                            <v-btn text><v-icon x-large color="white">{{ social.icon }}</v-icon></v-btn>
+                            <v-btn text>
+                                <v-icon x-large color="white">{{ social.icon }}</v-icon>
+                            </v-btn>
                         </v-col>
                     </v-row>
                 </v-col>
                 <v-col>
                     <p class="display-1 font-weight-bold text-capitalize">link penting</p>
                     <p>
-                        <span>Bantuan</span><br/>
-                        <span>Pusat Dokumen</span><br/>
-                        <span>Event dan Kegiatan</span><br/>
+                        <span>Bantuan</span><br />
+                        <span>Pusat Dokumen</span><br />
+                        <span>Event dan Kegiatan</span><br />
                     </p>
                 </v-col>
                 <v-col>
                     <p class="display-1 font-weight-bold text-capitalize">langganan berita dan makalah</p>
                     <p>
-                        Dapatkan informasi dan berita terbaru dari perusahaan kami. seperti berita, makalah, dan lain-lain.
+                        Dapatkan informasi dan berita terbaru dari perusahaan kami. seperti berita, makalah, dan
+                        lain-lain.
                     </p>
                     <v-text-field dense rounded outlined dark class="rounded-xl" label="alamat email"></v-text-field>
                     <v-checkbox dark label="Menerima syarat dan ketentuan kebijakan privasi">
                     </v-checkbox>
                 </v-col>
             </v-row>
-         </v-container>
-         <v-container fluid class="background-footer-bottom white--text">
+        </v-container>
+        <v-container fluid class="background-footer-bottom white--text">
             <div class="d-flex">
-                <span class="mr-auto">&copy; PT Prasimax Inovasi Teknologi, 2010 - {{ getYears }}, All rights reserved</span>
+                <span class="mr-auto">&copy; PT Prasimax Inovasi Teknologi, 2010 - {{ getYears }}, All rights
+                    reserved</span>
                 <span class="text-capitalize">Bergabung dengan Kami</span>
-                <v-btn x-small text v-for="(social, i) in socials" :key="i + 'social'"><v-icon color="white">{{ social.icon }}</v-icon></v-btn>
+                <v-btn x-small text v-for="(social, i) in socials" :key="i + 'social'">
+                    <v-icon color="white">{{ social.icon }}</v-icon>
+                </v-btn>
             </div>
-         </v-container>
+        </v-container>
     </div>
 </template>
 <script>
--- src/components/NavigationGuest.vue
+++ src/components/NavigationGuest.vue
@@ -6,49 +6,36 @@
                     <v-img src="@/assets/images/logo.png" max-width="300"></v-img>
                 </v-card>
                 <v-card flat class="pa-2">
-                    <v-text-field
-                    dense
-                    rounded
-                    outlined
-                    class="rounded-xl"
-                    label="Masukkan kata yang dicari"
-                    prepend-inner-icon="mdi-magnify"
-                    >
+                    <v-text-field dense rounded outlined class="rounded-xl" label="Masukkan kata yang dicari"
+                        prepend-inner-icon="mdi-magnify">
                     </v-text-field>
                 </v-card>
                 <v-card flat class="pa-2">
-                    <v-btn
-                    color="primary"
-                    class="rounded-xl"
-                    >
-                    <v-icon left>mdi-import</v-icon>
-                    Login
+                    <v-btn color="primary" class="rounded-xl">
+                        <v-icon left>mdi-import</v-icon>
+                        Login
                     </v-btn>
                 </v-card>
             </v-card>
         </v-container>
-            <v-app-bar dense flat dark>
-                <div v-for="(nav, idx) in menu" :key="idx + 'nav'">
-                    <v-btn text v-if="nav.submenu == undefined">{{ nav.name }}</v-btn>
-                    <v-menu offset-y v-else>
-                        <template v-slot:activator="{ on, attrs }">
-                            <v-btn
-                            text
-                            v-bind="attrs"
-                            v-on="on"
-                            >
+        <v-app-bar dense flat dark src="@/assets/images/dummy/rectangle.svg">
+            <div v-for="(nav, idx) in menu" :key="idx + 'nav'">
+                <v-btn text v-if="nav.submenu == undefined">{{ nav.name }}</v-btn>
+                <v-menu offset-y v-else>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn text v-bind="attrs" v-on="on">
                             {{ nav.name }}
-                            </v-btn>
-                        </template>
-                        <v-list>
-                            <v-list-item v-for="(sub, i) in nav.submenu" :key="i + 'submenu'">
-                                <v-list-item-title>{{ sub.name }}</v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-menu>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item v-for="(sub, i) in nav.submenu" :key="i + 'submenu'">
+                            <v-list-item-title>{{ sub.name }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
 
-                </div>
-            </v-app-bar>
+            </div>
+        </v-app-bar>
     </div>
 </template>
 <script>
