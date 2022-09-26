5ae7911ab94df9fec752fe8acf931b4987856c3f
Eshya <achmadayas@gmail.com>
2022-09-26 18:43:20 +0700

    fix config

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path= require('path');
 var app= express()
 
 // if (process.env.NODE_ENV === 'production') {
-app.use('/', express.static(process.env.PROJECT_DIST));
+app.use('/home', express.static(process.env.PROJECT_DIST));
 app.get('*', (req, res) => {
 res.sendFile(path.join(__dirname ,'dist', 'index.html'))
 // })
 });
-app.listen()
+
+module.exports = app
