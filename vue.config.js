fea8a9a83f530bdf4408c7b20a4a5eeb65853045
dhimaseka <dimaseka83@gmail.com>
2022-09-27 11:46:39 +0700

    ++

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,24 @@ module.exports = defineConfig({
 });
 
 module.exports = {
-  publicPath: '/',
   outputDir: 'dist',
-};
+  assetsDir: 'sub',
+  indexPath: 'index.html',
+  filenameHashing: true,
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Prasimax',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    },
+  },
+}
