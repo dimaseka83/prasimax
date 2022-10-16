9feb5567e8281bc084f02c7b8344b3389a0dd994
dimaseka83 <dimaseka83@gmail.com>
2022-10-16 20:46:44 +0700

    ++

--- src/page/Admin/Home.vue
+++ src/page/Admin/Home.vue
@@ -1,6 +1,34 @@
 <template>
     <v-app>
         <navigation-admin/>
+        <v-card class="my-16">
+            <v-data-table :headers="headercarousel" :items="carousel">
+                <template v-slot:top>
+                    <v-toolbar>
+                        <v-toolbar-title>Carousel <span class="subtitle-2 text-disabled">(Gambar harus svg)</span></v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-dialog v-model="dialogcarousel" max-width="500">
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn color="primary" class="mb-2" v-bind="attrs" v-on="on">Tambah</v-btn>
+                            </template>
+                            <v-card>
+                                <v-card-title>Buat Carousel Baru</v-card-title>
+                                <v-card-text>
+                                     <v-text-field label="Title" v-model="formcarousel.title"></v-text-field>
+                                    <v-text-field label="Description" v-model="formcarousel.subtitle"></v-text-field>
+                                    <v-file-input accept="image/*" v-model="formcarousel.image" label="Image"></v-file-input>
+                                </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="blue" text @click="dialogcarousel = false">Cancel</v-btn>
+                                <v-btn color="blue" text @click="savecarousel">Save</v-btn>
+                            </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                    </v-toolbar>
+                </template>
+            </v-data-table>
+        </v-card>
     </v-app>
 </template>
 <script>
@@ -14,7 +42,19 @@ export default {
     },
     data() {
         return {
+            dialogcarousel: false,
             carousel: [],
+            headercarousel: [
+                { text: 'Title', value: 'title' },
+                { text: 'Subtitle', value: 'subtitle' },
+                { text: 'Image', value: 'image' },
+                { text: 'Action', value: 'action' },
+            ],
+            formcarousel: {
+                title: '',
+                subtitle: '',
+                image: ''
+            }
         }
     },
     created() {
@@ -31,6 +71,24 @@ export default {
                 console.log(error)
             }
         },
+        async savecarousel(){
+            try{
+                await axios.post(`${this.apibe}carousel`,{
+                    withCredentials: true,
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${this.$store.state.token}`
+                    },
+            },this.formcarousel)
+                    .then(res => {
+                        console.log(res)
+                    })
+            } catch (error) {
+                console.log(error)
+            }
+
+        }
     },
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 export default {
     state: {
         user: [],
-        token: null,
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjcsInVzZXJuYW1lIjoiZGhpbXMxMjMzNDUiLCJlbWFpbCI6ImRoaW1hc19mZUBwcmFzaW1heC5jb20iLCJmdWxsbmFtZSI6ImRoaW1hcyIsInBlcnVzYWhhYW4iOiJsa2V3bmRsaWp3ZWQiLCJwcm9mZXNpIjoicGVydXNhaGFhbiIsInRlbGVwb24iOm51bGwsImlzU3Vic2NyaWJlIjpmYWxzZSwiaXNBY3RpdmUiOnRydWUsIkFjdGl2YXRlZHRva2VuIjpudWxsLCJyZXNldFBhc3N3b3JkVG9rZW4iOm51bGwsImNyZWF0ZWRBdCI6IjIwMjItMTAtMTZUMTI6NTA6MjcuMDAwWiIsInVwZGF0ZWRBdCI6IjIwMjItMTAtMTZUMTI6NTM6NDMuMDAwWiIsImlhdCI6MTY2NTkyNjMwOH0.r7TQ85EGZ-SotR2y0lz7YV-w45oIyUwFRNbd1E1sMNo",
         isLogged: false,
     },
     mutations: {
